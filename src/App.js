c78883aa5025af10668627030b3e917771037969
anshi06 <araghya6768@gmail.com>
2022-07-08 13:29:27 +0530

    React

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Banner from "./Components/Banner";
+import Navbar from "./Components/Navbar";
+import './App.css';
+import MovieList from "./Components/MovieList";
+
+
+function App() {
+  return (
+    <div>
+     <Navbar/>
+     <Banner/>
+     <MovieList/>
+    </div>
+  );
+}
+
+export default App;
