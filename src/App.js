b7f4a84819dfadfce2658d78ec8dd4be4aef5dcf
anshi06 <araghya6768@gmail.com>
2022-07-23 20:49:12 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
+import './App.css';
 import Banner from "./Components/Banner";
 import Navbar from "./Components/Navbar";
-import './App.css';
 import MovieList from "./Components/MovieList";
+import Favourites from "./Components/Favourites";
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 
 function App() {
   return (
-    <div>
-     <Navbar/>
-     <Banner/>
-     <MovieList/>
-    </div>
+    <BrowserRouter>
+    <Navbar />
+
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <>
+            <Banner />
+            <MovieList />
+          </>
+        }
+      />
+
+      <Route path="/favourites" element={<Favourites />} />
+    </Routes>
+  </BrowserRouter>
   );
 }
 
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react'
 
-export class Navbar  extends Component {
+
+import {Link} from 'react-router-dom'
+
+export class Navbar extends Component {
   render() {
     return (
-      <div style={{display : 'flex', alignItems :'center'}}>
-        <h1>Movies App</h1>
-        <h2 style={{marginLeft :"2rem", marginTop :"0.7rem" }}>Favourites</h2>
+      <div style={{display : 'flex' , alignItems:'center'}}>
+        <Link to='/'> <h1>Movies App</h1> </Link>
+        <Link to='favourites'><h2 style={{marginLeft:"2rem" , marginTop :'0.7rem'}}>Favourites</h2></Link>
       </div>
     )
   }
 }
 
-export default Navbar 
+export default Navbar
